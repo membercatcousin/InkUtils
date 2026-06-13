8a4dc937b871d8b3d03ff9bc1895b0b0b79d49ca
membercatcousin <membercatcousin@gmail.com>
2026-06-13 20:23:27 +0100

    Rename command from 'dont_ask' to 'dataja'

--- commands/community/dataja.py
+++ commands/community/dataja.py
@@ -4,6 +4,6 @@ from discord.ext import commands
 from bootstrap.bot_boot import *
 
 # Simple command that replies with a short, fixed message.
-@bot.hybrid_command(name="dont_ask", description="Don't ask to ask. Just ask!")
+@bot.hybrid_command(name="dataja", description="Don't ask to ask. Just ask!")
 async def dont_ask(ctx):
     await ctx.send("Don't ask to ask. Just ask!")
