056188dc809b9bd90853a9a063f672c384094fc0
membercatcousin <membercatcousin@gmail.com>
2026-06-13 20:42:58 +0100

    Implement bot version and presence update on ready
    Add versioning and on_ready event for bot presence.

--- bootstrap/bot_boot.py
+++ bootstrap/bot_boot.py
@@ -2,6 +2,8 @@ import discord
 from bootstrap.intents import *
 from discord.ext import commands
 
+version = "v1.0.0"
+
 # Custom bot subclass for SmokeUtils.
 class SmokeUtils(commands.Bot):
     def __init__(self):
@@ -17,4 +19,12 @@ class SmokeUtils(commands.Bot):
         print("commands synced")
 
 bot = SmokeUtils()
+
+@bot.event
+async def on_ready():
+    # Called once when the bot successfully connects to Discord.
+    global server_count
+    server_count = len(bot.guilds)
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(f'SmokeUtils Community Edition {version}'))
+    print("running")
 
