a29054d6d192abd340f2b54f3c91cff4d66ae8c5
membercatcousin <membercatcousin@gmail.com>
2026-06-13 20:35:36 +0100

    update the status

--- bootstrap/is_ready.py
+++ bootstrap/is_ready.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 from bootstrap.bot_boot import *
 
 # Bot version display used in presence text.
-version = "v0.1.0"
+version = "v1.0.0"
 
 @bot.event
 async def on_ready():
     # Called once when the bot successfully connects to Discord.
     global server_count
     server_count = len(bot.guilds)
-    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(f'SmokeUtils Lite {version}'))
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(f'SmokeUtils Community Edition {version}'))
     print("running")
