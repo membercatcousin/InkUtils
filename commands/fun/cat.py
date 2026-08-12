c3468590025c47d46e1d2f13355a901f0ad822e2
membercatcousin <membercatcousin@proton.me>
2026-08-13 00:05:06 +0100

    a

---
+++ commands/fun/cat.py
@@ -0,0 +1,24 @@
+import discord
+import random
+from bootstrap.intents import *
+from discord.ext import commands
+from bootstrap.bot_boot import *
+
+# List of cat image URLs used by the cat command.
+cats = [
+    "https://cataas.com/cat",
+    "https://cataas.com/cat/orange",
+    "https://cataas.com/cat/says/hi",
+    "https://cataas.com/cat/says/hello",
+    "https://cataas.com/cat/says/meow",
+    "https://cataas.com/cat/says/purr",
+    "https://cataas.com/cat/says/miw",
+    "https://cataas.com/cat/says/nya",
+    "https://cataas.com/cat/says/nyaa",
+    "https://cataas.com/cat/cute",
+]
+
+@bot.hybrid_command(name="cat", description="Send a random cat picture")
+async def cat(ctx):
+    # Reply with a random cat image URL from the list.
+    await ctx.reply(random.choice(cats))
